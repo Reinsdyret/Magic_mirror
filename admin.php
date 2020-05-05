f07af30e762c4163ff0bcf91f58880f46c7b0d69
Lars Møen Haukland <Lars.haukland@gmail.com>
2020-05-05 22:30:49 +0200

    Add files via upload

---
+++ admin.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<?php
+#require "ting/connect.php";
+echo "<header class=\"header\">";
+include "ting/header.php";
+echo "</header>";
+echo "<footer class=\"footer\">";
+include "ting/footer.html";
+echo "</footer>";
+?>
+<html>
+<head>
+    <title>AdminPage</title>
+    <link rel="stylesheet" type="text/css" href="style/admin_style.css">
+</head>
+<body>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<?php
+#require "ting/connect.php";
+include "ting/header.php";
+echo "<footer class=\"footer\">";
+include "ting/footer.html";
+echo "</footer>";
+?>
+<html>
+<head>
+    <title>HomePage</title>
+    <link rel="stylesheet" type="text/css" href="style/style.css">
+</head>
+<body>
+    <p class="info">
+        HeiHei<br>
+        Dis is ours bewtiful miror!
+    </p>
+    <img src="media/logo.png" class="logo" alt="LogoSpeil" height="400">
+</body>
+
+</html>
