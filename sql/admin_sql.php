1940ee0abce71dfdfcb6ba8e0ec4c73b6f91ce5e
Lars Møen Haukland <Lars.haukland@gmail.com>
2020-05-22 10:50:14 +0200

    Update admin_sql.php

--- sql/admin_sql.php
+++ sql/admin_sql.php
@@ -37,12 +37,31 @@ $vær_box = $_POST["vær_box"];
 $id = $_SESSION["id"];
 
 
-$sql = "INSERT INTO mmwidget_i_bruk (person_id, type, box, bruk)
-    VALUES 
-        ($id, \"huskeliste\", $huskeliste_box, $check_huskeliste),
-        ($id, \"nyheter\", $nyheter_box, $check_nyheter),
-        ($id, \"timeplan\", $timeplan_box, $check_timeplan),
-        ($id, \"vær\", $vær_box, $check_vær)";
-
+$sql = "SELECT * from mmwidget_i_bruk WHERE person_id = $id"
 $result = mysqli_query($conn,$sql);
+
+
+if($result){
+    $sql = "UPDATE mmwidget_i_bruk SET type = \"huskeliste\", box = $huskeliste_box, bruk = $check_huskeliste WHERE person_id = $id AND type = \"huskeliste\"";
+    $result = mysqli_query($conn,$sql);
+
+    $sql = "UPDATE mmwidget_i_bruk SET type = \"nyheter\", box = $nyheter_box, bruk = $check_nyheter WHERE person_id = $id AND type = \"nyheter\"";
+    $result = mysqli_query($conn,$sql);
+
+    $sql = "UPDATE mmwidget_i_bruk SET type = \"timeplan\", box = $timeplan_box, bruk = $check_timeplan WHERE person_id = $id AND type = \"timeplan\"";
+    $result = mysqli_query($conn,$sql);
+
+    $sql = "UPDATE mmwidget_i_bruk SET type = \"vær\", box = $vær_box, bruk = $check_vær WHERE person_id = $id AND type = \"vær\"";
+    $result = mysqli_query($conn,$sql);
+}
+
+else{
+    $sql = "INSERT INTO mmwidget_i_bruk (person_id, type, box, bruk)
+            VALUES 
+            ($id, \"huskeliste\", $huskeliste_box, $check_huskeliste),
+            ($id, \"nyheter\", $nyheter_box, $check_nyheter),
+            ($id, \"timeplan\", $timeplan_box, $check_timeplan),
+            ($id, \"vær\", $vær_box, $check_vær)";
+
+    $result = mysqli_query($conn,$sql);
 ?>
