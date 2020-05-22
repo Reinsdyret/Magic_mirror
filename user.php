184774e80fe51d103aaac207f8efe57105e3c1a8
Lars Møen Haukland <Lars.haukland@gmail.com>
2020-05-22 11:05:34 +0200

    Update user.php

--- user.php
+++ user.php
@@ -2,50 +2,31 @@
 <?php
 session_start();
 include "ting/connect_local.php";
+
 $id = $_SESSION["id"];
 
+$box_row = [0,1,2,3,4];
+$box_column = [0,1,2,3,4];
 
 include "sql/user_sql.php";
-$time = True;
-$husk = True;
-$klokke = True;
 
 while($row = mysqli_fetch_assoc($result)){
-    if($row["type"] == "timeplan" && $row["bruk"] && $time){
-        $box = $row["box"];
-        $time = False;
-        echo "<nav class=\"timeplan\">";
-        echo "<iframe class=\"timeplan\" src=\"http://www.novasoftware.se/webviewer/(S(fwngwce3fquowo45yat4cx45))/design1.aspx?schoolid=60810&code=545354&type=3&id=231\" height=\"500px\"></iframe>";
-        echo "</nav>";
-      }
-      if($row["type"] == "huskeliste" &&$row["bruk"] && $husk){
-        include "sql/huskeliste.php";
-        echo "<nav class=\"huskeliste\">";
-        echo "<h2>Huskeliste</h2>";
-        echo "<ul>";
-        while($row_husk = mysqli_fetch_assoc($result_husk)){
-          echo "<li>";
-          echo  $row_husk["status"];
-          echo "</li>";
-        }
-        echo "</ul>";
-        echo "</nav>";
-    }
-    if($row["type"] == "klokke" && $row["bruk"] && $klokke){
-      include "ting/klokke.html";
-      $klokke = False;
+    if($row["type"] == "timeplan" && $row["bruk"]){
+      $sql2 = "SELECT link FROM mmtimeplan WHERE person_id = $id";
+      $result2 = mysqli_query($conn,$sql2);
+      $rowt = mysqli_fetch_assoc($result2);
+      $box = $row["box"];
+      echo "<iframe src=$rowt[\"link\"]></iframe>";
     }
 }
 
 
 ?>
-<!DOCTYPE html>
 <html>
   <head>
     <title>Speil</title>
     <meta charset="utf-8">
-    <link rel="stylesheet" type="text/css"  href="style/user_style.css">
-    <link rel="stylesheet" type="text/css" href="style/user.css.php" media="screen">
+    <link rel="stylesheet" type="text/css"  href="ting/user_style.css">
   </head>
   <body>
   </body>
